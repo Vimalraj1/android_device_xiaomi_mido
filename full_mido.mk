0a5758e980be35e6ac464212c53e865ce499d8d8
SayanAcharya <rowdysayan99@gmail.com>
2018-01-01 10:18:06 +0000

    QCOM: Add QCOM Utils

--- full_mido.mk
+++ full_mido.mk
@@ -21,6 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
+# Call QCOM Utils
+$(call inherit-product, $(DEVICE_PATH)/utils.mk)
+
 # Device identifier. This must come after all inclusions
 TARGET_VENDOR := Xiaomi
 PRODUCT_DEVICE := mido
